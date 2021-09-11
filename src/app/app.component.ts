2ddd9196ff97460e45a0a195f6046c97965141a7
JimmyAnso <jimmy123698745@yahoo.com.tw>
2021-09-11 16:14:31 +0800

    feat: addToDo、hasBeenDone、removeToDo、enterPress 功能
    addToDo：使用 ViewChild, ElementRef 取得 .nativeElement.value
    hasBeenDone：使用 (e.target as HTMLElement).classList 操控樣式
    removeToDo ：帶入 *ngFor 的 index 並從 this.list 中刪除
    enterPress：enterPress(e: KeyboardEvent) 當 e.key == "Enter" 觸發 this.addToDo();

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild("my_input", { static: true }) myInput!: ElementRef;
   title = "My To Do List";
   list: any[] = ["Hit the gym", "Pay bills", "Meet George", "Buy eggs", "Read a book", "Organize office"];
+
+  enterPress(e: KeyboardEvent) {
+    console.log(e.key);
+    if (e.key == "Enter") {
+      this.addToDo();
+    }
+  }
+  addToDo() {
+    var inputValue = this.myInput.nativeElement.value;
+    if (inputValue == "") {
+      alert("You must write something!");
+    }
+    else {
+      this.list.push(inputValue);
+      this.myInput.nativeElement.value = "";
+    }
+  }
+
+  hasBeenDone(e: Event) {
+    var classList = (e.target as HTMLElement).classList;
+    if (classList[0] == 'checked') {
+      classList.remove("checked");
+    }
+    else {
+      classList.add("checked");
+    }
+  }
+
+  removeToDo(removeIndex: number) {
+    this.list.splice(removeIndex, 1);
+  }
 }
